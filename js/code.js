892bf125b5ce30e65064d954015f259c189d2f7c
SanyFaysal <tfoysalahmed@gmail.com>
2022-03-02 20:58:31 +0600

    comment added

--- js/code.js
+++ js/code.js
@@ -1,21 +1,26 @@
+
 const spinner = document.getElementById('spinner');
+// spinner toggle 
 const toggle = (displaySpinner) => {
     spinner.style.display = displaySpinner;
 }
 toggle('none', 'inline');
+// load searching phone 
 const loadPhone = async () => {
     toggle('inline');
     const search = document.getElementById('searchText');
     const searchText = search.value;
     search.value = '';
-
+    // search error handle 
     if (searchText == "oppo" || searchText == "huawei" || searchText == "apple") {
+        // load result 
         const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         const res = await fetch(url);
         const data = await res.json();
         displayPhone(data.data)
 
     }
+    // error messege 
     else {
         const container = document.getElementById('error');
         const div = document.createElement('div');
@@ -29,15 +34,19 @@ const loadPhone = async () => {
     }
 
 }
+// display searching results 
 const displayPhone = phones => {
     toggle('none')
+    // clear error messege 
     const error = document.getElementById('error');
     error.textContent = '';
     const container = document.getElementById('displayPhones');
     container.textContent = "";
+    // get all results from phones array 
     phones.forEach(phone => {
         const div = document.createElement('div');
-        div.classList.add('col')
+        div.classList.add('col');
+        // display single result in a card  
         div.innerHTML = `
     <div class="card w-100 p-4 mb-3 w-md-50 shadow border-0 rounded-3 " >
             <div class="px-4 pt-3 rounded">
@@ -47,7 +56,7 @@ const displayPhone = phones => {
           <h5 class="card-title pt-0 mt-0">${phone.phone_name}</h5>
           <p class="card-text">${phone.brand}</p>
           <a href="#showDetails" class="text-none">
-          <button onclick="loadDetails('${phone.slug}')" class="bg-primary border-0 text-white py-1 px-4  rounded ">Details</button>
+          <button onclick="loadDetails('${phone.slug}')" class="bg-danger border-0 fw-bold text-white py-1 px-4 rounded ">Details</button>
           </a>
         </div>
       </div>
@@ -56,7 +65,7 @@ const displayPhone = phones => {
 
     })
 }
-
+// load details 
 const loadDetails = async (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
@@ -64,9 +73,10 @@ const loadDetails = async (id) => {
     displayDetails(data.data)
 
 }
-
+// display phone details 
 const displayDetails = details => {
     const handleOthers = () => {
+        // handle details error 
         if (!details.others) {
             return "Not available";
         }
@@ -74,7 +84,7 @@ const displayDetails = details => {
             return (typeof (feature) == 'string' ? details.others : 'Not available');
         }
     }
-
+    // show sensor 
     const getSensors = details.mainFeatures.sensors;
     const [sens1, sens2, sens3, sens4, sens5, sens6] = getSensors;
 
@@ -83,6 +93,7 @@ const displayDetails = details => {
     const div = document.createElement('div');
     div.classList.add('d-flex');
     div.classList.add('justify-content-center');
+    // show all details 
     div.innerHTML = `
         <div class="card mb-3 d-inline">
             <div class="row g-0 mt-3">
